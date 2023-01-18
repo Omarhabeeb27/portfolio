c9c4ed2017341598d95449137fea46c004d92c0f
Omarhabeeb27 <oamrhabeeb242@gmail.com>
2023-01-18 21:04:04 +0530

    commit

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,23 +1,51 @@
-import { NavBar, Container } From "react-bootstrap";
+import { useEffect, useState } from "react";
+import { NavBar, Container } from "react-bootstrap";
 
 export const NavBar = () => {
+    const [activeLink, setActiveLink] = useState('home');
+    const [scrolled, seScrolled] = useState(False);
+  
+    useEffect(() => {
+    const onScroll = () => {
+        if (window.scrollY > 50) {
+            setScrolled(true);  }
+
+        else {
+            setScrolled(flase);
+        }
+    }
+
+    window.addEventListener("scroll", onScroll);
+
+    reutrn () => window.removeEventListener("scroll", onScroll);
+
+
+    }, [] )
+
+
     return(
-     <Navbar bg="light" expand="lg">
+     <Navbar expand="lg" className = { scrolled ? "scrolled": " "}>
         <Container>
-            <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
-            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Brand href="#home">
+                <img src={''} alt="logo" />
+            </Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" >
+                <span className ="navbar-toggle-icon"></span>
+            </Navbar.Toggle>
             <Navbar.Collapse id="basic-navbar-nav">
-             <Nav className="me-auto">
-                <Nav.Link href="#home">Home</Nav.Link>
-                <Nav.Link href="#link">Link</Nav.Link>
-                <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-                  <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                  <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-                  <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-                  <NavDropdown.Divider />
-                  <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
-            </NavDropdown>
+              <Nav className="me-auto">
+                <Nav.Link href="#home" className = { activeLink=== 'home' ? 'active navbar-link' : 'navbar-link'}>Home</Nav.Link>
+                <Nav.Link href="#skills" className = { activeLink=== 'skills' ? 'active navbar-link' : 'navbar-link'}>skills</Nav.Link>
+                <Nav.Link href="#projects" className = { activeLink=== 'projects' ? 'active navbar-link' : 'navbar-link'}>projects</Nav.Link>
           </Nav>
+          <span className="navbar-text">
+            <div className="social-icon">
+                <a href="#"><img src="{}" alt="" /></a>
+                <a href="#"><img src="{}" alt="" /></a>
+                <a href="#"><img src="{}" alt="" /></a>
+            </div>
+            <button className="vvd" onClick={() => console.log('connect')}><span>Lets's Connect</span></button>
+          </span>
         </Navbar.Collapse>
       </Container>
     </Navbar>
